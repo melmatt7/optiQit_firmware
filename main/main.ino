ef505dccff2009d33bbb95c4e64c11fcefbc1da8
melmatt7 <mel.matt007@gmail.com>
2020-01-28 02:39:26 -0800

    progress

--- main/main.ino
+++ main/main.ino
@@ -11,7 +11,7 @@
 #include "my_PCNT.h"
 #include "my_ble.h"
 
-#define WAIT_MS  10 // Time to wait (in milliseconds) before reporting pulse count reading
+#define WAIT_MS  1000 // Time to wait (in milliseconds) before reporting pulse count reading
 
 
 void setup() {
@@ -23,14 +23,35 @@ void setup() {
 void loop() {  
   int16_t count_0 = 0;                      // 16-bit count register
   int16_t count_1 = 0;                      // 16-bit count register
-  if (pcnt_init_and_start() != ESP_OK){     // Start counting pulses
-      Serial.printf("\nFAILED: pcnt_init_and_start\n");
-  } else {
-    Serial.printf("\nCounter started\n");
+
+  // notify changed value
+  if (deviceConnected) {
+      pCharacteristic->setValue((uint8_t*)&value, 4);
+      pCharacteristic->notify();
+      value++;
+      delay(3); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+      
+      if (pcnt_init_and_start() != ESP_OK){     // Start counting pulses
+        Serial.printf("\nFAILED: pcnt_init_and_start\n");
+      } else {
+        Serial.printf("\nCounter started\n");
+      }
+      delay(WAIT_MS);                              
+      pcnt_get(&count_0, &count_1);
+      Serial.printf("\nCurrent counter_0 value :%d", count_0);
+      Serial.printf("\nCurrent counter_1 value :%d\n", count_1);
+      pcnt_clear();
   }
-  delay(WAIT_MS);                              
-  pcnt_get(&count_0, &count_1);
-  Serial.printf("\nCurrent counter_0 value :%d", count_0);
-  Serial.printf("\nCurrent counter_1 value :%d\n", count_1);
-  pcnt_clear();
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+      delay(500); // give the bluetooth stack the chance to get things ready
+      pServer->startAdvertising(); // restart advertising
+      Serial.println("start advertising");
+      oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+      // do stuff here on connecting
+      oldDeviceConnected = deviceConnected;
+  }
 }
