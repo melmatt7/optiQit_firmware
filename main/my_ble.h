e8b26649d2786870b3448f4f81370885c9b378c1
serena-ramley <sramley@gmail.com>
2020-01-30 20:58:41 -0800

    Added a second pulse counter

--- main/my_ble.h
+++ main/my_ble.h
@@ -14,14 +14,17 @@
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
+BLEServer *pServer;
+BLEService *pService;
+BLECharacteristic *pCharacteristic;
+
 static void BLEInit() {
-    Serial.begin(115200);
     BLEDevice::init("Long name works now");
     Serial.println("Starting BLE work!");
 
-    BLEServer *pServer = BLEDevice::createServer();
-    BLEService *pService = pServer->createService(SERVICE_UUID);
-    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+    *pServer = BLEDevice::createServer();
+    *pService = pServer->createService(SERVICE_UUID);
+    *pCharacteristic = pService->createCharacteristic(
                                            CHARACTERISTIC_UUID,
                                            BLECharacteristic::PROPERTY_READ |
                                            BLECharacteristic::PROPERTY_WRITE
